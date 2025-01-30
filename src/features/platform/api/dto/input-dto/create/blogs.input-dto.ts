5a607027a3ad65a48a41fef7a11182c0e7711ce1
Yauheni Kryzhyk <krizhik@gmail.com>
2025-01-30 22:28:57 +0300

    get back validation

--- src/features/platform/api/dto/input-dto/create/blogs.input-dto.ts
+++ src/features/platform/api/dto/input-dto/create/blogs.input-dto.ts
@@ -1,20 +1,22 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { CreateBlogDto } from '../../../../dto/create/create-blog.dto';
 import { blogValidationRules } from '../../../../domain/validation-rules';
+import { IsStringWithTrim } from '../../../../../../core/decorators/validation/is-string-with-trim';
+import { Matches } from 'class-validator';
 
 export class CreateBlogInputDto implements CreateBlogDto {
     @ApiProperty({
         maxLength: blogValidationRules.name.maxLength,
         required: true,
     })
-    // @IsStringWithTrim(1, blogValidationRules.name.maxLength)
+    @IsStringWithTrim(1, blogValidationRules.name.maxLength)
     name: string;
 
     @ApiProperty({
         maxLength: blogValidationRules.description.maxLength,
         required: true,
     })
-    // @IsStringWithTrim(1, blogValidationRules.description.maxLength)
+    @IsStringWithTrim(1, blogValidationRules.description.maxLength)
     description: string;
 
     @ApiProperty({
@@ -22,7 +24,7 @@ export class CreateBlogInputDto implements CreateBlogDto {
         pattern: String(blogValidationRules.websiteUrl.pattern),
         required: true,
     })
-    // @Matches(blogValidationRules.websiteUrl.pattern)
-    // @IsStringWithTrim(1, blogValidationRules.websiteUrl.maxLength)
+    @Matches(blogValidationRules.websiteUrl.pattern)
+    @IsStringWithTrim(1, blogValidationRules.websiteUrl.maxLength)
     websiteUrl: string;
 }
