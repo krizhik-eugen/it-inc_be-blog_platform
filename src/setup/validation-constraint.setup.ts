fb2cf097c77d52dd1a85a8e55ac744504990b214
yauheni-deriv <yauheni@regentmarkets.com>
2025-02-17 11:55:46 +0300

    validationConstraintSetup translate description

--- src/setup/validation-constraint.setup.ts
+++ src/setup/validation-constraint.setup.ts
@@ -2,7 +2,7 @@ import { DynamicModule, INestApplication } from '@nestjs/common';
 import { useContainer } from 'class-validator';
 
 /**
- * Для внедрения зависимостей в validator constraint decorator
+ * For dependency injection in validator constraint decorators
  * @param app
  * @param DynamicAppModule
  */
@@ -10,8 +10,8 @@ export const validationConstraintSetup = (
     app: INestApplication,
     DynamicAppModule: DynamicModule,
 ) => {
-    // {fallbackOnErrors: true} требуется, поскольку Nest генерирует исключение,
-    // когда DI не имеет необходимого класса.
+    // {fallbackOnErrors: true} is required because Nest generates an exception
+    // when DI doesn't have the necessary class.
     const appContext = app.select(DynamicAppModule);
 
     useContainer(appContext, {
