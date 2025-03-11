7914ff054e9ab5bd44f64f38655895e70ccacac3
yauheni-deriv <yauheni@regentmarkets.com>
2025-03-11 12:08:06 +0300

    fix me view data and get sessions

--- src/modules/accounts/infrastructure/queryRepositories/sessions.postgres-query-repository.ts
+++ src/modules/accounts/infrastructure/queryRepositories/sessions.postgres-query-repository.ts
@@ -1,7 +1,9 @@
+import { Injectable } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { PostgresSessionViewDto } from '../../api/dto/view-dto/sessions.view-dto';
 import { PostgresSession } from '../../domain/session.postgres-entity';
 
+@Injectable()
 export class PostgresSessionsQueryRepository {
     constructor(private dataSource: DataSource) {}
 
@@ -9,7 +11,7 @@ export class PostgresSessionsQueryRepository {
         userId: number,
     ): Promise<PostgresSessionViewDto[]> {
         const sessions: PostgresSession[] = await this.dataSource.query(
-            `SELECT * FROM sessions WHERE user_id = $1`,
+            `SELECT * FROM sessions WHERE user_id = $1 AND deleted_at IS NULL`,
             [userId],
         );
         return sessions.map((session) =>
