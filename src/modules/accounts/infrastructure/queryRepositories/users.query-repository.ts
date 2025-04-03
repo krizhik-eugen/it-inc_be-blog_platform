de3017a9618ae4a3edaf2e547c9dd66844bf2556
Yauheni Kryzhyk <krizhik@gmail.com>
2025-04-03 21:08:52 +0300

    users query repo refactor

--- src/modules/accounts/infrastructure/queryRepositories/users.query-repository.ts
+++ src/modules/accounts/infrastructure/queryRepositories/users.query-repository.ts
@@ -1,101 +1,152 @@
 import { Injectable } from '@nestjs/common';
-import { DataSource } from 'typeorm';
+import { DataSource, IsNull, Repository } from 'typeorm';
 import {
     PaginatedUsersViewDto,
     MeViewDto,
     UserViewDto,
 } from '../../api/dto/view-dto';
 import { NotFoundDomainException } from '../../../../core/exceptions';
-import { User } from '../../domain/user.entity';
+import { User, UserEntity } from '../../domain/user.entity';
 import {
     GetUsersQueryParams,
     UsersSortBy,
 } from '../../api/dto/query-params-dto';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersQueryRepository {
-    constructor(private dataSource: DataSource) {}
+    constructor(
+        @InjectRepository(UserEntity)
+        private readonly usersRepo: Repository<UserEntity>,
+        private dataSource: DataSource,
+    ) {}
 
     async getCurrentUserByIdOrNotFoundFail(id: number) {
-        const data: User[] = await this.dataSource.query(
-            `
-                SELECT * FROM public.users
-                WHERE id = $1 AND deleted_at IS NULL
-            `,
-            [id],
-        );
+        const data: UserEntity | null = await this.usersRepo.findOneBy({
+            id,
+            deleted_at: IsNull(),
+        });
 
-        if (!data.length) {
+        if (!data) {
             throw NotFoundDomainException.create('User is not found');
         }
 
-        return MeViewDto.mapToView(data[0]);
+        return MeViewDto.mapToView(data);
     }
 
     async getByIdOrNotFoundFail(id: number): Promise<UserViewDto> {
-        const data: User[] = await this.dataSource.query(
-            `
-                SELECT * FROM public.users
-                WHERE id = $1 AND deleted_at IS NULL
-                `,
-            [id],
-        );
+        const data: UserEntity | null = await this.usersRepo.findOneBy({
+            id,
+            deleted_at: IsNull(),
+        });
 
-        if (!data.length) {
+        if (!data) {
             throw NotFoundDomainException.create('User is not found');
         }
 
-        return UserViewDto.mapToView(data[0]);
+        return UserViewDto.mapToView(data);
     }
+
+    // async getAllUsers(
+    //     query: GetUsersQueryParams,
+    // ): Promise<PaginatedUsersViewDto> {
+    //     const queryParams: (string | number)[] = [];
+    //     let paramIndex = 1;
+
+    //     const searchLoginTerm = query.searchLoginTerm
+    //         ? query.searchLoginTerm
+    //         : null;
+    //     const searchEmailTerm = query.searchEmailTerm
+    //         ? query.searchEmailTerm
+    //         : null;
+
+    //     let sqlQuery = `
+    //         WITH filtered_users AS (
+    //             SELECT * FROM users
+    //             WHERE deleted_at IS NULL
+    //     `;
+
+    //     if (searchLoginTerm) {
+    //         sqlQuery += ` AND login ILIKE $${paramIndex}`;
+    //         queryParams.push(`%${searchLoginTerm}%`);
+    //         paramIndex++;
+    //     }
+
+    //     if (searchEmailTerm) {
+    //         sqlQuery += ` ${searchLoginTerm ? 'OR' : 'AND'} email ILIKE $${paramIndex}`;
+    //         queryParams.push(`%${searchEmailTerm}%`);
+    //         paramIndex++;
+    //     }
+
+    //     sqlQuery += `
+    //         )
+    //         SELECT
+    //             filtered_users.*,
+    //             COUNT(*) OVER() AS total_count
+    //         FROM filtered_users
+    //         ORDER BY ${this.sanitizeSortField(query.sortBy)} ${query.sortDirection}
+    //         LIMIT $${paramIndex} OFFSET $${paramIndex + 1}
+    //     `;
+
+    //     queryParams.push(query.pageSize, query.calculateSkip());
+
+    //     const result: Array<User & { total_count: string }> =
+    //         await this.dataSource.query(sqlQuery, queryParams);
+
+    //     const totalCount = Number(result[0]?.total_count) || 0;
+    //     const mappedUsers = result.map((user) => UserViewDto.mapToView(user));
+
+    //     return PaginatedUsersViewDto.mapToView({
+    //         items: mappedUsers,
+    //         page: query.pageNumber,
+    //         size: query.pageSize,
+    //         totalCount: totalCount,
+    //     });
+    // }
 
     async getAllUsers(
         query: GetUsersQueryParams,
     ): Promise<PaginatedUsersViewDto> {
-        const queryParams: (string | number)[] = [];
-        let paramIndex = 1;
+        const searchLoginTerm = query.searchLoginTerm || null;
+        const searchEmailTerm = query.searchEmailTerm || null;
 
-        const searchLoginTerm = query.searchLoginTerm
-            ? query.searchLoginTerm
-            : null;
-        const searchEmailTerm = query.searchEmailTerm
-            ? query.searchEmailTerm
-            : null;
+        const qb = this.usersRepo
+            .createQueryBuilder('users')
+            .where('users.deleted_at IS NULL');
 
-        let sqlQuery = `
-            WITH filtered_users AS (
-                SELECT * FROM users
-                WHERE deleted_at IS NULL
-        `;
+        if (searchLoginTerm && searchEmailTerm) {
+            qb.andWhere(
+                '(users.login ILIKE :loginTerm OR users.email ILIKE :emailTerm)',
+                {
+                    loginTerm: `%${searchLoginTerm}%`,
+                    emailTerm: `%${searchEmailTerm}%`,
+                },
+            );
+        } else {
+            if (searchLoginTerm) {
+                qb.andWhere('users.login ILIKE :loginTerm', {
+                    loginTerm: `%${searchLoginTerm}%`,
+                });
+            }
 
-        if (searchLoginTerm) {
-            sqlQuery += ` AND login ILIKE $${paramIndex}`;
-            queryParams.push(`%${searchLoginTerm}%`);
-            paramIndex++;
+            if (searchEmailTerm) {
+                qb.andWhere('users.email ILIKE :emailTerm', {
+                    emailTerm: `%${searchEmailTerm}%`,
+                });
+            }
         }
 
-        if (searchEmailTerm) {
-            sqlQuery += ` ${searchLoginTerm ? 'OR' : 'AND'} email ILIKE $${paramIndex}`;
-            queryParams.push(`%${searchEmailTerm}%`);
-            paramIndex++;
-        }
+        const sortField = this.sanitizeSortField(query.sortBy);
+        qb.orderBy(
+            `users.${sortField}`,
+            query.sortDirection.toUpperCase() as 'ASC' | 'DESC',
+        );
 
-        sqlQuery += `
-            )
-            SELECT 
-                filtered_users.*,
-                COUNT(*) OVER() AS total_count
-            FROM filtered_users
-            ORDER BY ${this.sanitizeSortField(query.sortBy)} ${query.sortDirection}
-            LIMIT $${paramIndex} OFFSET $${paramIndex + 1}
-        `;
+        qb.skip(query.calculateSkip()).take(query.pageSize);
 
-        queryParams.push(query.pageSize, query.calculateSkip());
+        const [users, totalCount] = await qb.getManyAndCount();
 
-        const result: Array<User & { total_count: string }> =
-            await this.dataSource.query(sqlQuery, queryParams);
-
-        const totalCount = Number(result[0]?.total_count) || 0;
-        const mappedUsers = result.map((user) => UserViewDto.mapToView(user));
+        const mappedUsers = users.map((user) => UserViewDto.mapToView(user));
 
         return PaginatedUsersViewDto.mapToView({
             items: mappedUsers,
